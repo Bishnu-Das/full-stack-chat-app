d0a76df6277ad56ab8a9fd7b1602ab1692f080e9
Bishnu Das <bishnudas1315@gmail.com>
2025-05-17 17:58:46 +0600

    third time

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,12 +34,13 @@ function App() {
       {console.log(document.documentElement.getAttribute("data-theme"))}
 
       <Routes>
-        <Route path = "/" element={authUser?< HomePage/>:<Navigate to="/login" />}/>
-        <Route path = "/signup" element={!authUser?<SignupPage/>:<Navigate to="/"/>}/>
-        <Route path = "/login" element={!authUser?<LoginPage/>:<Navigate to="/"/>}/>
-        <Route path = "/settings" element={<SettingPage/>}/>
-        <Route path = "/profile" element={authUser?<ProfilePage/>:<Navigate to="/login" />}/>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
       <Toaster/>
 
     </div>
